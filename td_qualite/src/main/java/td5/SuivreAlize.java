39d6e4417fe86a6e0663c404a4ebfc39c8e31034
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:27:59 +0100

    ajout de la documentation de SuivreAlize package td5

--- td_qualite/src/main/java/td5/SuivreAlize.java
+++ td_qualite/src/main/java/td5/SuivreAlize.java
@@ -1,8 +1,17 @@
 package td5;
 
-public class SuivreAlize implements SuivreRoute{
+/**
+ * Implémentation de l'interface {@link SuivreRoute} représentant une stratégie de suivi de route "Alizé".
+ * Cette classe définit le comportement spécifique du suivi de route pour le vent Alizé.
+ */
+public class SuivreAlize implements SuivreRoute {
 
-    public void suivreRoute(){
+    /**
+     * Méthode qui imprime le suivi de la route utilisant la stratégie Alizé.
+     * Affiche "Alize" pour indiquer que la route suit le vent Alizé.
+     */
+    @Override
+    public void suivreRoute() {
         System.out.println("Alize");
     }
 }
