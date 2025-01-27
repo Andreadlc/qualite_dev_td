f52c288143b7c118c074a77cb71e7a31e7b463e9
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:01:11 +0100

    ajout de la javadoc et de la documentation pour categorie du package observateur

--- doc/type-search-index.js
+++ doc/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"p":"observateur","l":"Abandon"},{"l":"All Classes and Interfaces","u":"allclasses-index.html"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"observateur","l":"Categorie"}];updateSearchResults();
--- td_qualite/src/main/java/observateur/Categorie.java
+++ td_qualite/src/main/java/observateur/Categorie.java
@@ -3,29 +3,56 @@ package observateur;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Categorie implements Observateur{
+/**
+ * Représente une catégorie qui observe les abandons liés à une classe spécifique.
+ */
+public class Categorie implements Observateur {
 
-    td7.Categorie nom_categorie;
-    List<Abandon> abandons;
+    /**
+     * Nom de la catégorie.
+     */
+    private final td7.Categorie nom_categorie;
 
+    /**
+     * Liste des abandons associés à cette catégorie.
+     */
+    private final List<Abandon> abandons;
 
-    public Categorie(td7.Categorie nom_categorie){
+    /**
+     * Constructeur pour créer une catégorie.
+     *
+     * @param nom_categorie le nom de la catégorie associée
+     */
+    public Categorie(td7.Categorie nom_categorie) {
         this.nom_categorie = nom_categorie;
-        abandons = new ArrayList<>();
+        this.abandons = new ArrayList<>();
     }
 
+    /**
+     * Méthode appelée lorsqu'un observable est mis à jour.
+     * Ajoute un abandon à la liste des abandons si sa classe correspond à celle de la catégorie.
+     *
+     * @param observable l'objet observable à partir duquel la mise à jour est déclenchée
+     */
     @Override
     public void actualise(Observable observable) {
         DirectionCourse direction = (DirectionCourse) observable;
         Abandon abandon = direction.getLastAbandon();
-        System.out.println("actualise "+ abandon.getClasse()+ " " + nom_categorie);
-        if(abandon.getClasse().equals(this.nom_categorie.toString())){
+        System.out.println("actualise " + abandon.getClasse() + " " + nom_categorie);
+
+        if (abandon.getClasse().equals(this.nom_categorie.toString())) {
             abandons.add(abandon);
         }
-
     }
 
-    public String toString(){
+    /**
+     * Retourne une représentation textuelle de la catégorie, incluant son nom,
+     * le nombre d'abandons et la liste des abandons.
+     *
+     * @return une chaîne de caractères représentant la catégorie
+     */
+    @Override
+    public String toString() {
         return nom_categorie + " " + abandons.size() + " " + abandons;
     }
 }
