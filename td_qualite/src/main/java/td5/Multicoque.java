7332a66bc48d082655d30d2ac4e70f875281d322
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:26:50 +0100

    ajout de la documentation de multicoque package td5

--- td_qualite/src/main/java/td5/Multicoque.java
+++ td_qualite/src/main/java/td5/Multicoque.java
@@ -1,18 +1,37 @@
 package td5;
 
-public class Multicoque extends Voilier{
+/**
+ * Représente un voilier de type Multicoque.
+ * Hérite de la classe {@link Voilier}.
+ */
+public class Multicoque extends Voilier {
 
-    public Multicoque(String nom, SuivreRoute suivreRoute){
-
-        super(nom,suivreRoute);
+    /**
+     * Constructeur pour créer un Multicoque avec un nom et une stratégie de suivi de route.
+     *
+     * @param nom         le nom du multicoque
+     * @param suivreRoute la stratégie de suivi de route associée au multicoque
+     */
+    public Multicoque(String nom, SuivreRoute suivreRoute) {
+        super(nom, suivreRoute);
     }
 
-    public Multicoque(String nom){
+    /**
+     * Constructeur pour créer un Multicoque avec un nom uniquement.
+     *
+     * @param nom le nom du multicoque
+     */
+    public Multicoque(String nom) {
         super(nom);
     }
 
+    /**
+     * Retourne une représentation textuelle du multicoque.
+     *
+     * @return une chaîne de caractères représentant le multicoque
+     */
     @Override
     public String toString() {
-        return super.toString() + " "+"Multicoque";
+        return super.toString() + " " + "Multicoque";
     }
 }
