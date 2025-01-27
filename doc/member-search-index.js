8298d8d10b68d7dd2468bd3b5d774ae5faed5a10
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:19:12 +0100

    ajout de la javadoc et de la documentation de l'interface observateur du package observateur

--- doc/member-search-index.js
+++ doc/member-search-index.js
@@ -1,1 +1,1 @@
-memberSearchIndex = [{"p":"observateur","c":"Observable","l":"desenregistreObservateur(Observateur)","u":"desenregistreObservateur(observateur.Observateur)"},{"p":"observateur","c":"Observable","l":"enregistreObservateur(Observateur)","u":"enregistreObservateur(observateur.Observateur)"},{"p":"observateur","c":"Observable","l":"informeObservateurs()"}];updateSearchResults();
+memberSearchIndex = [{"p":"observateur","c":"Observateur","l":"actualise(Observable)","u":"actualise(observateur.Observable)"}];updateSearchResults();
--- td_qualite/src/main/java/observateur/Observateur.java
+++ td_qualite/src/main/java/observateur/Observateur.java
@@ -1,5 +1,16 @@
 package observateur;
 
+/**
+ * Interface représentant un observateur dans le modèle Observateur.
+ * Les classes qui implémentent cette interface peuvent être notifiées
+ * des changements d'état des objets observables auxquels elles sont enregistrées.
+ */
 public interface Observateur {
+
+    /**
+     * Méthode appelée lorsque l'objet observable subit un changement d'état.
+     *
+     * @param observable l'objet observable qui a changé d'état
+     */
     public void actualise(Observable observable);
 }
