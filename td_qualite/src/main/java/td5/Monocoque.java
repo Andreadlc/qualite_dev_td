2545f4b657be83b5af1db8fc131785077b5bdefe
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:26:04 +0100

    ajout de la documentation de monocoque package td5

--- td_qualite/src/main/java/td5/Monocoque.java
+++ td_qualite/src/main/java/td5/Monocoque.java
@@ -1,17 +1,37 @@
 package td5;
 
+/**
+ * Représente un voilier de type Monocoque.
+ * Hérite de la classe {@link Voilier}.
+ */
 public class Monocoque extends Voilier {
 
-    public Monocoque (String nom, SuivreRoute suivreRoute){
-        super(nom,suivreRoute);
+    /**
+     * Constructeur pour créer un Monocoque avec un nom et une stratégie de suivi de route.
+     *
+     * @param nom         le nom du monocoque
+     * @param suivreRoute la stratégie de suivi de route associée au monocoque
+     */
+    public Monocoque(String nom, SuivreRoute suivreRoute) {
+        super(nom, suivreRoute);
     }
 
-    public Monocoque (String nom){
+    /**
+     * Constructeur pour créer un Monocoque avec un nom uniquement.
+     *
+     * @param nom le nom du monocoque
+     */
+    public Monocoque(String nom) {
         super(nom);
     }
 
+    /**
+     * Retourne une représentation textuelle du monocoque.
+     *
+     * @return une chaîne de caractères représentant le monocoque
+     */
     @Override
     public String toString() {
-        return super.toString() + " "+"Monoque";
+        return super.toString() + " " + "Monocoque";
     }
 }
