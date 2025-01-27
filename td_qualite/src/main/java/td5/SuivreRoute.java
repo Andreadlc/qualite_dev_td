dfebf5b93645de175e66866981e5363324f82b18
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:30:20 +0100

    ajout de la documentation de l'interface SuivreRoute package td5

--- td_qualite/src/main/java/td5/SuivreRoute.java
+++ td_qualite/src/main/java/td5/SuivreRoute.java
@@ -1,7 +1,14 @@
 package td5;
 
+/**
+ * Interface représentant une stratégie de suivi de route.
+ * Les classes qui implémentent cette interface définissent des comportements spécifiques de suivi de route.
+ */
 public interface SuivreRoute {
 
+    /**
+     * Méthode pour suivre une route en fonction de la stratégie définie.
+     * Chaque implémentation de cette méthode doit spécifier le comportement du suivi de la route.
+     */
     public void suivreRoute();
 }
-
