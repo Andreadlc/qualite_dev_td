9b3507e566745693f654d6230134681325e664b3
Andreadlc <andreadelcros7@gmail.com>
2025-01-27 18:29:03 +0100

    ajout de la documentation de SuivreOrthodromie package td5

--- td_qualite/src/main/java/td5/SuivreOrthodromie.java
+++ td_qualite/src/main/java/td5/SuivreOrthodromie.java
@@ -1,8 +1,17 @@
 package td5;
 
-public class SuivreOrthodromie implements SuivreRoute{
+/**
+ * Implémentation de l'interface {@link SuivreRoute} représentant une stratégie de suivi de route par orthodromie.
+ * Cette classe définit le comportement spécifique du suivi de route pour l'orthodromie, qui est la route la plus courte entre deux points sur une sphère.
+ */
+public class SuivreOrthodromie implements SuivreRoute {
 
-    public void suivreRoute(){
-        System.out.println("Orthodomie");
+    /**
+     * Méthode qui imprime le suivi de la route utilisant la stratégie Orthodromie.
+     * Affiche "Orthodomie" pour indiquer que la route suit la trajectoire orthodromique.
+     */
+    @Override
+    public void suivreRoute() {
+        System.out.println("Orthodromie");
     }
 }
